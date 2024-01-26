fb259d5a3b8aabe8eab0c77efb42d7e3ffbfb58b
Aminejebara <aminebenjebara5@gamil.com>
2024-01-26 15:17:25 +0100

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
-app.listen(3000, () => {
-  console.log("server is runinig on port 3000");
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(5000, () => {
+  console.log("server connected to 5000");
 });
