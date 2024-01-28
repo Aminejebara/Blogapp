96d16581146827854af8e6fa32ca95909fe40a95
Aminejebara <aminebenjebara5@gamil.com>
2024-01-28 11:45:51 +0100

    test api

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,3 @@
+export const test = (req, res) => {
+  res.json({ message: "me me me" });
+};
--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+
 dotenv.config();
 const app = express();
 
@@ -16,3 +18,5 @@ mongoose
 app.listen(5000, () => {
   console.log("server connected to 5000");
 });
+
+app.use("/api/user", userRoutes);
